e181a67e8149e051d58b50df333c7f37cc0bc630
KAPOZING <duo5913@naver.com>
2019-07-10 22:23:58 +0900

    GD project

--- GDProject_FinitDark/Scene/CharacterScene/Player.gd
+++ GDProject_FinitDark/Scene/CharacterScene/Player.gd
@@ -1,13 +1,18 @@
 extends KinematicBody2D
 
-const hAccel = 6
-const hDecel = 10
-const hMAX_SPEED = 60
+const WALK_SPEED = 100
+const SPRINT_SPEED = 120
+const AIR_SPEED = 80
+
+const hAccel = 0.1
+const hDecel = 0.2
+
 const GRAVITY = 7
 const JUMP_POWER = -200
 const FLOOR = Vector2(0, -1)
 
 var velocity = Vector2()
+var hMOVE_SPEED = null
 
 var onGround = false
 var jumpCount = 2
@@ -24,32 +29,32 @@ func _physics_process(delta):
 
 func character_movement():
 	
+	if onGround == true:
+		hMOVE_SPEED = WALK_SPEED
+	elif onGround == false:
+		hMOVE_SPEED = AIR_SPEED
+	
 	#Horizontal Move
 	if Input.is_action_pressed("ui_right"):
-		facing = 1
-		if velocity.x < hMAX_SPEED:
-			velocity.x += hAccel 
+		velocity.x = lerp(velocity.x, hMOVE_SPEED, hAccel)
+		if onGround == true:
+			facing = 1
 	elif Input.is_action_pressed("ui_left"):
-		facing = -1
-		if velocity.x > -hMAX_SPEED:
-			velocity.x -= hAccel 
+		velocity.x = lerp(velocity.x, -hMOVE_SPEED, hAccel)
+		if onGround == true:
+			facing = -1
 	else:
-		if velocity.x > 0:
-			velocity.x -= hDecel
-		elif velocity.x < 0:
-			velocity.x += hDecel
-		else:
+		velocity.x = lerp(velocity.x, 0, hDecel)
+		if facing > 0 && velocity.x <= 0.5:
+			velocity.x = 0
+		elif facing < 0 && velocity.x >= -0.5:
 			velocity.x = 0
 	
 	#Jump
-	if Input.is_action_pressed("ui_up"):
+	if Input.is_action_pressed("ui_A"):
 		if onGround == true:
 			velocity.y = JUMP_POWER
 			onGround == false
-		elif jumpCount > 0:
-			velocity.y = JUMP_POWER
-			jumpCount -= 1
-			print("double jump")
 	
 	velocity.y += GRAVITY
 	
@@ -60,23 +65,34 @@ func character_movement():
 		onGround = false
 	
 	
+	
 	velocity = move_and_slide(velocity, FLOOR)
 
 
 func character_animation():
 	
-	if facing < 0 && velocity.x > 0:
-		$AnimatedSprite.play("Turn")
-	elif facing > 0 && velocity.x < 0:
-		$AnimatedSprite.play("Turn")
 	
 	if onGround == true && facing < 0:
 		$AnimatedSprite.flip_h = true
+		if facing > 0:
+			$AnimatedSprite.play("Turn")
 	elif onGround == true && facing > 0:
 		$AnimatedSprite.flip_h = false
+		if facing < 0:
+			$AnimatedSprite.play("Turn")
 	
-	if onGround == true && velocity.x != 0:
+	
+	if onGround == true && Input.is_action_pressed("ui_right") || Input.is_action_pressed("ui_left"):
 		$AnimatedSprite.play("MoveHorizontal")
-	else:
+	elif onGround == true:
 		$AnimatedSprite.play("IdleHorizontal")
 	
+	
+	if onGround == false:
+		if Input.is_action_pressed("ui_A"):
+			$AnimatedSprite.play("JumpStart")
+			if velocity.y > 0:
+				$AnimatedSprite.play("JumpEnd")
+		elif velocity.y <= 0:
+			$AnimatedSprite.play("Falling")
+		
