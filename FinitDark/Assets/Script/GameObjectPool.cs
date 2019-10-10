8beae752c85346dd2f5fdeb51e4b6a3c7ce2516a
pepsiricano <pepsiricano@gmail.com>
2019-10-10 19:30:22 +0900

    Updated

---
+++ FinitDark/Assets/Script/GameObjectPool.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectPool : MonoBehaviour
+{
+    private static GameObjectPool mInstance;
+    public static GameObjectPool Instance{
+        get {
+            if (mInstance == null)
+            {
+                mInstance = FindObjectOfType(typeof(GameObjectPool)) as GameObjectPool;
+            }
+            return mInstance;
+        }
+    }
+
+    public GameObject Pridy;
+}
---
+++ FinitDark/Assets/Script/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float Speed;
+
+    private void Awake()
+    {
+        
+    }
+
+    private IEnumerator Start()
+    {
+        while(true)
+        {
+            float horizontal = Input.GetAxis("Horizontal");
+            float vertical = Input.GetAxis("Vertical");
+
+            horizontal = horizontal * Speed * Time.deltaTime;
+            vertical = vertical * Speed * Time.deltaTime;
+            transform.Translate(Vector2.right * horizontal);
+            transform.Translate(Vector2.up * vertical);
+            yield return null;
+        }
+    }
+}
